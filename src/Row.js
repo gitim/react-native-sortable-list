9ef4123c3eef29f1d11ff36c3dd6f2b0e80efebb
Timur Gibadullin <tim.gibadullin@gmail.com>
2017-04-04 21:37:59 +0300

    Add horizontal direction support

--- src/Row.js
+++ src/Row.js
@@ -9,6 +9,7 @@ export default class Row extends Component {
     children: PropTypes.node,
     animated: PropTypes.bool,
     disabled: PropTypes.bool,
+    horizontal: PropTypes.bool,
     style: Animated.View.propTypes.style,
     location: PropTypes.shape({
       x: PropTypes.number,
@@ -194,9 +195,9 @@ export default class Row extends Component {
   }
 
   _mapGestureToMove(prevGestureState, gestureState) {
-    return {
-      dy: gestureState.moveY - prevGestureState.moveY,
-    };
+    return this.props.horizontal
+      ? {dx: gestureState.moveX - prevGestureState.moveX}
+      : {dy: gestureState.moveY - prevGestureState.moveY};
   }
 
   _isDisabled() {
