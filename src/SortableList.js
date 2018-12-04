9d75ed7eda7c84defe310f8d4d4ba030c8dfa42e
Lucas Coelho <lucascoelhodacosta@gmail.com>
2018-12-04 10:28:57 -0200

    Wrap scrollToBottom logic to a separated function

--- src/SortableList.js
+++ src/SortableList.js
@@ -331,14 +331,17 @@ export default class SortableList extends Component {
             contentWidth,
           }, () => {
             this.setState({ animated: true });
-            setTimeout(() => {
-              if (this.state.data.length > 0) {
-                this.scrollToRowKey({ key: this.state.data.length - 1, animated: true });
-              }
-            }, 100);
           });
         });
       });
+  }
+
+  scrollToBottom = () => {
+    setTimeout(() => {
+      if (this.state.data.length > 0) {
+        this.scrollToRowKey({ key: this.state.data.length - 1, animated: true });
+      }
+    }, 100);
   }
 
   _scroll(animated) {
